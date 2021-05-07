1a4061de1a55d0927590b51789be780cc8150700
Borisigle <borisiglesias1@gmail.com>
2021-05-06 21:12:43 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const addTask = (task) => {
 
   return (
     <div className="container">
-      <Header title='Hello World!' toggleAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      <Header title='Task Manager' toggleAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
       {showAddTask && <AddTask onAdd= {addTask} />}
       {tasks.length > 0 ? <Tasks task= {tasks} onDelete= {deleteTask} onToggle={toggleReminder}/> : 'No task to show'}
       
